0d638dda77190d84835f6b63cb9bdc78df45baf0
Robert Danci <robertda@monday.com>
2024-10-16 16:44:48 +0100

    take & play sound

--- app/camera_service.py
+++ app/camera_service.py
@@ -1,15 +1,19 @@
 import imageio
 import imageio as iio
 import matplotlib.pyplot as plt
+from matplotlib import image as mpimg
+
 
 def take_photo():
-    camera = iio.get_reader("<video8>")
-    screenshot = camera.get_data(0)
-    camera.close()
-    imageio.imwrite("/tmp/test.jpg", screenshot)
-
+    # camera = iio.get_reader("<video8>")
+    # screenshot = camera.get_data(0)
+    # camera.close()
+    # imageio.imwrite("/tmp/test.jpg", screenshot)
+    #
     plt.clf()
-    plt.imshow(screenshot)
+    img = mpimg.imread('IMG.jpg')
+    # Display the image
+    plt.imshow(img)
     plt.show(block=False)
 
 
