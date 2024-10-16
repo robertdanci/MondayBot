3d9a5dcc02f269d31773f1c8637422b0f39726e9
ibukun oluwayomi <ibukunol@monday.com>
2024-10-16 13:34:59 +0100

    implements scaffolding for webhook handler and challenge response

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
-from app.audio_service import play_audio
-from app.camera_service import take_photo
+from audio_service import play_audio
+from camera_service import take_photo
 
 app = FastAPI()
 
@@ -14,4 +15,51 @@ def read_root():
 def play():
     play_audio()
     return "done"
+
+"""
+{
+  app: 'monday',
+  type: 'update_column_value',
+  triggerTime: '2024-10-16T11:44:03.781Z',
+  subscriptionId: 420586429,
+  userId: 43066799,
+  originalTriggerUuid: null,
+  boardId: 7410328198,
+  groupId: 'group_title',
+  pulseId: 7410328521,
+  pulseName: 'Item 5',
+  columnId: 'status',
+  columnType: 'color',
+  columnTitle: 'Status',
+  value: {
+    label: { index: 1, text: 'Done', style: [Object], is_done: true },
+    post_id: null
+  },
+"""
+class Event(BaseModel):
+    app: str
+    type: str
+    triggerTime: str
+    subscriptionId: int
+    userId: int
+    boardId: int
+    groupId: str
+    pulseId: int
+    pulseName: str
+    columnId: str
+    columnType: str
+    columnTitle: str
+
+class WebHookBody(BaseModel):
+    event: Event | None = None
+    challenge: str | None = None
+
+@app.post("/webhook")
+def webhook_handler(body: WebHookBody):
+    print(body)
+    if body.challenge:
+        print("handling challenge scenario")
+        return {"challenge": body.challenge}
+    print("handling event")
+    return "ok"
 
