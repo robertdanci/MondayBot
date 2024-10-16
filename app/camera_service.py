d6110797c055ac0b6aada6fd6c2e3bf7fc26b8c6
Robert Danci <robertda@monday.com>
2024-10-16 15:28:58 +0100

    remove size

--- app/camera_service.py
+++ app/camera_service.py
@@ -5,7 +5,7 @@ pygame.init()
 pygame.camera.init()
 
 def take_photo():
-    cam = pygame.camera.Camera('/dev/video8', (500, 500))
+    cam = pygame.camera.Camera('/dev/video8')
     cam.start()
     image = cam.get_image()
     pygame.image.save(image, "/tmp/captured_image.jpg")
