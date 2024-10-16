f41e8ef98faddffaa7a5d1586883dade8a89abe9
Robert Danci <robertda@monday.com>
2024-10-16 13:27:49 +0100

    update play method

--- app/audio_service.py
+++ app/audio_service.py
@@ -1,5 +1,7 @@
 from pydub import AudioSegment
+from pydub.playback import play
+
 
 def play_audio():
     song = AudioSegment.from_wav("403057__vesperia94__hooray.wav")
-    song.play()
+    play(song)
