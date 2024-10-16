57f69753b551f06c9354dff37a49e2142c9e14a2
Robert Danci <robertda@monday.com>
2024-10-16 16:23:39 +0100

    clear and show

--- app/camera_service.py
+++ app/camera_service.py
@@ -1,3 +1,4 @@
+import imageio
 import imageio as iio
 import matplotlib.pyplot as plt
 
@@ -5,6 +6,7 @@ def take_photo():
     camera = iio.get_reader("<video8>")
     screenshot = camera.get_data(0)
     camera.close()
+    imageio.imwrite("/tmp/test.jpg", screenshot)
 
     plt.clf()
     plt.imshow(screenshot)
